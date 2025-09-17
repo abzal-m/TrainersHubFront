047fdc2f148009adeb1cd21f759a30502bb933c2
AbzalMautaliyev <abzalmautaliev@gmail.com>
2025-09-18 00:00:47 +0500

    Добавил дашборд

--- src/model/router.ts
+++ src/model/router.ts
@@ -3,11 +3,13 @@ import LandingPage from "@/pages/LandingPage.vue";
 import Index from "@/pages/Index.vue";
 import TestStrava from "@/pages/TestStrava.vue";
 import AuthForm from "@/pages/AuthForm.vue";
+import AthleteDashboard from "@/pages/AthleteDashboard.vue";
 
 export const defineRoutes: RouteRecordRaw[] =  [
   { path: "/", name: "Index", component: Index },
   { path: "/LandingPage", name: "LandingPage", component: LandingPage },
   { path: "/AuthForm", name: "AuthForm", component: AuthForm },
+  { path: "/AthleteDashboard", name: "AthleteDashboard", component: AthleteDashboard },
   { path: "/TestStrava", name: "TestStrava", component: TestStrava },
 ];
 
@@ -15,5 +17,6 @@ export enum Routes {
     Index = 'Index',
     LandingPage = 'LandingPage',
     AuthForm = 'AuthForm',
+    AthleteDashboard = 'AthleteDashboard',
     TestStrava = 'TestStrava',
 }
---
+++ src/pages/AthleteDashboard.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="card dock-demo">
+    <div class="dock-window">
+      <Dock :model="items" :position="position" :pt="{
+        item: 'px-2',
+        itemlink: 'w-5rem h-4rem'
+      }">
+        <template #itemicon="{ item }">
+          <i :class="item.icon" style="font-size: 2.1rem; color: #615e5e" @click="onDockItemClick(item?.label)"></i>
+        </template>
+      </Dock>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {ref} from "vue";
+
+const onDockItemClick = (item: string) => {
+  console.log(item)
+};
+
+const items = ref([
+  {
+    label: 'Home',
+    icon: 'pi pi-home',
+    title: 'Главная',
+  },
+  {
+    label: 'Calendar',
+    icon: 'pi pi-calendar',
+    title: 'Календарь',
+
+  },
+  {
+    label: 'Statistics',
+    icon: 'pi pi-chart-line',
+    title: 'Статистика',
+  },
+  {
+    label: 'Profile',
+    icon: 'pi pi-user',
+    title: 'Профиль',
+  }
+]);
+const position = ref('bottom');
+
+</script>
+
+<style scoped>
+.dock-demo > .dock-window {
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  background-color: #cdcdcd;
+  background-repeat: no-repeat;
+  background-size: cover;
+  z-index: 1;
+}
+
+.dock-demo > .p-dock {
+  z-index: 1000;
+}
+</style>
